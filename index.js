82838c53c1c64208c8bd5e48cedf865d559e2631
liugege <2212722889@qq.com>
2022-10-10 17:46:28 +0800

    完成引用,图片,有序列表的一部分

--- index.js
+++ index.js
@@ -1,43 +1,65 @@
 let fs = require("fs");
 const data = fs.readFileSync("./js_markdown.md", "utf-8");
 htmlRes(data);
-var htmlArr =[]
-var htmlData = [] //html元素数组
+var htmlArr = [];
+var htmlData = []; //html元素数组
 //判断是不是####
 function htmlRes(data) {
-  if(htmlArr === undefined)htmlArr = []
+  if (htmlArr === undefined) htmlArr = [];
   htmlArr = data.split("\n");
-  htmlArr.forEach((item,index)=>{
-    item = item.trim()
+  htmlArr.forEach((item, index) => {
+    item = item.trim();
     //标题
-    if(item !== '\n' && item.length >1){
-        let titleRes = retrunTitle(item)
-        if(titleRes){
-          htmlData.push(titleRes)    
-        }   
+    if (item !== "\n" && item.length > 1) {
+      let titleRes = retrunTitle(item);
+      if (titleRes) {
+        htmlData.push(titleRes);
+      }
     }
     // //超链接 字符必须超过6个
-    if(item.length > 6){
-        ifLink(item)
+    if (item.length > 6) {
+      ifLink(item);
     }
     //引用语法
-    if(item.length>2 && item[0] == '>'){
-        htmlData.push(returnQuote(item,index))
+    if (item.length > 2 && item[0] == ">") {
+      htmlData.push(returnQuote(item, index));
     }
-  })
+    //图片
+    if (item.length > 5 && item[0] == "!") {
+      returnImg(item);
+    }
+    //判断有序列表
+    // console.log((item[0]-0))
+    if ('0123456789'.indexOf(item[0] - 0)!==-1) {
+      let arr = htmlArr[index].split('')
+      let num = 0
+      let flag = true
+      let i = 0
+      while(flag){
+        if(arr[i] === ' '){
+          num = num+1
+        }else{
+          flag = false
+        }
+        i=i+1
+      }
+      console.log(num,'连续的空格数')
+      // returnOl(item,index);
+    }
+  });
 }
 
 //返回标题
-function retrunTitle(item){
-    // 1-7 #
-    for (let i = 7; i >= 1; i--) {
-      if (item.indexOf(returnFor(i)) !== -1 && item[0] === '#') {
-        if(htmlData === undefined)htmlData = []
-        //这里就是 查询的字符的索引然后加上这个标题#的个数就是内容
-        return createElement(item.indexOf(returnFor(i)) + i, item)
-      }
+function retrunTitle(item) {
+  // 1-7 #
+  for (let i = 7; i >= 1; i--) {
+    if (item.indexOf(returnFor(i)) !== -1 && item[0] === "#") {
+      if (htmlData === undefined) htmlData = [];
+      //这里就是 查询的字符的索引然后加上这个标题#的个数就是内容
+      return createElement(item.indexOf(returnFor(i)) + i, item);
     }
-  return false
+  }
+  return false;
 }
 
 //生成 1-7 个#
@@ -57,81 +79,160 @@ function createElement(num, item) {
 
 //判断是不是超链接--------------------------------------------------------------
 function ifLink(content) {
-  content = content.trim()
+  content = content.trim();
   // []()
   let arr = ["[", "]", "(", ")"];
-  let arr2 = []
+  let arr2 = [];
   let flag = 0;
   //循环查询
-  arr.forEach(item=>{
-    if(content.indexOf(item) !==-1){
-      arr2.push(content.indexOf(item))
-      flag++
+  arr.forEach((item) => {
+    if (content.indexOf(item) !== -1) {
+      arr2.push(content.indexOf(item));
+      flag++;
     }
-  })
+  });
   //四个都有
-  if(flag === 4){
-      if(arr2[0] < arr2[1] && arr2[1]+1 === arr2[2] && arr2[3] == content.length-1){
-         //这个时候就是超链接 -----提取内容[a标签内容] (链接)
-         let aText = content.slice(arr2[0]+1,arr2[1])
-         let link = content.slice(arr2[2]+1,arr2[3])
-         if(htmlData === undefined)htmlData = []
-         htmlData.push(`<a href="${link}" alt="图片">${aText}</a>`)
-      }
-  }else{
-    return
+  if (flag === 4) {
+    if (
+      arr2[0] < arr2[1] &&
+      arr2[1] + 1 === arr2[2] &&
+      arr2[3] == content.length - 1
+    ) {
+      //这个时候就是超链接 -----提取内容[a标签内容] (链接)
+      let aText = content.slice(arr2[0] + 1, arr2[1]);
+      let link = content.slice(arr2[2] + 1, arr2[3]);
+      if (htmlData === undefined) htmlData = [];
+      htmlData.push(`<a href="${link}" alt="图片">${aText}</a>`);
+    }
+  } else {
+    return;
   }
 }
 
 //------------------------------引用语法------------------------------------
-function returnQuote(content,index = null){
-  content = content.trim()
-  console.log(content,index,htmlArr,'全部数据')
-  let flag = true
-  let i = index +1
-  let successCount = 0
-  while(flag){
-     if(htmlArr[i]){
-        let str = htmlArr[i]
-        if( str[0] === '>'){
-          //连续的引用数量
-          successCount++
-          console.log('+l',successCount)
-          i++
-        }else{
-          flag = false
-        }         
-     }
-     flag = false
+function returnQuote(content, index = null) {
+  content = content.trim();
+  let arrHtml = [content.slice(1).trim()]; //引用的数据
+  let {successCount} = returnContinu(index,arrHtml,'>')
+  let htmlStr = createElementQ("div", { class: "quote" }, "div");
+  //处理引用
+  for (let i = 0; i < arrHtml.length; i++) {
+    arrHtml[i] = arrHtml[i].trim();
+    if (retrunTitle(arrHtml[i])) {
+      //引用+标题
+      arrHtml[i] = retrunTitle(arrHtml[i]);
+    } else {
+      //这里没有标题
+      let ele = createElementQ("p", {}, "p");
+      ele.splice(1, 0, arrHtml[i]);
+      arrHtml[i] = ele.join("");
+    }
   }
-  console.log(successCount,'数量')
-  if(successCount){
-    //有连续的时候
-      for(let i =0;i<=successCount;i++){
-        if(returnTitle(htmlArr[index+i])){
-          
-        }
-      }
-      htmlArr.splice(index+1,successCount)
-      console.log(htmlArr)
-  }else{
-    //没有连续的时候
-  }
-  // if(content[1] === ' '){//长度必须大于2
-  //   // console.log(content,'content')
-  //   let titleRes = retrunTitle(content.slice(2))
-  //   if(titleRes){//引用标题
-  //       return `<div class="quote">${titleRes}</div>`
-  //   }
-  //   //单纯引用
-  //   return `<div class="quote">${content.slice(2)}</div>`
-  // }
-  return false
+  htmlArr.splice(index + 1, successCount);
+  htmlStr.splice(1,0,arrHtml.join(''))
+  htmlData = htmlData.concat(htmlStr.join(''));
+  return false;
 }
 
-//创建标签
-function createElementQ(name,classContent,content){
-  let arr1 = [`<${name} class="${classContent}">`]
-  let arr2 = [`</${name}>`]
-  return arr1.concat(arr2)
+//----------------------------图片语法--------------------------------
+function returnImg(item) {
+  console.log("这是传进来的", item);
+  item = item.trim();
+  let rule = {
+    0: "!",
+    1: "[",
+    2: "]",
+    3: "(",
+    4: ")",
+  };
+  let r0 = item.indexOf(rule["0"]);
+  let r1 = item.indexOf(rule["1"], r0);
+  let r2 = item.indexOf(rule["2"], r1);
+  let r3 = item.indexOf(rule["3"], r2);
+  let r4 = item.indexOf(rule["4"], r3);
+  let alt = "";
+  let src = "";
+  let imgTitle = "";
+  if (
+    item[0] === rule["0"] &&
+    r1 < r2 &&
+    r2 < item.indexOf(r3) &&
+    r3 < r4 &&
+    item[item.length - 1] === rule["4"]
+  ) {
+    alt = item.slice(r1 + 1, r2);
+    let str = item.slice(r3 + 1, r4);
+    if (str.indexOf(" ") !== -1) {
+      src = str.split(" ")[0];
+      imgTitle = str.split(" ")[1];
+    }
+    htmlData.push(createElementQ("img", { alt, src, title: imgTitle }).join(''));
+  }
 }
+
+//----------------------------无序列表-------------------------------------
+function returnOl(content,index) {
+  content = content.trim();
+  let arrHtml = [content]; 
+  let {successCount} = returnContinu(index,arrHtml,'1234567890',0)
+  let htmlStr = createElementQ("ol", { }, "ol");
+  arrHtml.forEach((item,i)=>{
+    arrHtml[i]  = createElementQ('li',{},'li')
+    arrHtml[i].splice(1,0,item)
+    arrHtml[i] =arrHtml[i].join(' ')
+  })
+  htmlArr.splice(index + 1, successCount);
+  htmlStr.splice(1,0,arrHtml.join(''))
+  htmlData = htmlData.concat(htmlStr.join(''));
+}
+
+
+
+
+
+
+//创建标签
+function createElementQ(name, classObj) {
+  let keys = Object.keys(classObj);
+  let str = "";
+  keys.forEach((item) => {
+    str += `${item}='${classObj[item]}' `;
+  });
+  let arr1 = [`<${name} ${str}>`];
+  let arr2 = [`</${name}>`];
+  return arr1.concat(arr2);
+}
+
+/**
+ * 
+ * @param {*} index 当前位置索引
+ * @returns {
+ *    successCount 连续的个数
+ * label 开头标签
+ * }
+ */
+function returnContinu(index,arrHtml,label,num=2){
+  let flag = true;
+  let i = index + 1; //初始当前数据的下一条
+  let successCount = 0; //要删除的数量
+  while (flag) {
+    if (htmlArr[i]) {
+      let str = htmlArr[i][0];
+      if (label.indexOf(str) !==-1) {
+        //连续的引用数量
+        arrHtml.push(htmlArr[i].slice(num));
+        i = i + 1;
+        successCount = successCount + 1;
+      } else {
+        flag = false;
+        continue;
+      }
+    } else {
+      flag = false;
+    }
+  }
+  return {
+      successCount
+  }
+}
+//多行连续的方法例:(引用,有无序列表)
